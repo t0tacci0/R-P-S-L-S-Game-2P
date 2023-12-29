75b561aa8e6df1259feaccb01e161e2d17233060
t0tacci0 <>
2023-12-29 02:39:30 +0000

    Addanimation shacking style for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,1 @@
-const computer = document.querySelector("#player0 img");
-const player = document.querySelector("#player1 img");
-const computerScore = document.querySelector("#computer-score");
-const playerScore = document.querySelector("#player-score");
-const buttons = document.querySelectorAll("#buttons-container button");
+
