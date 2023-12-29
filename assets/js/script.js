1827807524b8dcd63a00de9ba54bc39e4f330b42
t0tacci0 <>
2023-12-29 00:59:59 +0000

    Add const to start game

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const computer = document.querySelector("#player0 img");
+const player = document.querySelector("#player1 img");
+const computerScore = document.querySelector("#computer-score");
+const playerScore = document.querySelector("#player-score");
+const buttons = document.querySelectorAll("#buttons-container button");
