7a1cb3d29b3497bb261be1e2520b47ae38a66170
t0tacci0 <>
2023-12-29 02:47:17 +0000

    Add shacking function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 
+    const computer = document.querySelector(".player0 img");
+    const player = document.querySelector(".player1 img");
+    const computerScore = document.querySelector(".computer-score");
+    const playerScore = document.querySelector(".player-score");
+    const buttons = document.querySelectorAll(".buttons-container button");
+
+
+function shacking() {
+    buttons.forEach((option) => {
+        option.addEventListener("click", () => {
+            computer.classList.add("shakeComputer");
+            player.classList.add("shakePlayer");
+        });
+    });
+}
+shacking();
