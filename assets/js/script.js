4e0f0b4bd4c2ac44390f5d7ad971354197704fb1
t0tacci0 <>
2024-01-03 20:14:25 +0000

    Add lizard and spock const

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ const mainGame = () => {
         const rock = document.querySelector(".rock");
         const paper = document.querySelector(".paper");
         const scissors = document.querySelector(".scissors");
+        const lizard = document.querySelector(".lizard");
+        const spock = document.querySelector(".spock");
 
         startBtn.addEventListener("click", () => {
             startScreen.style.visibility = "hidden";
